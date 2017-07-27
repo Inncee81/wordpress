0336947b5621068b7b233c917056fe0b4eb56c3c
wu <wuguojun@chinatelecom.cn>
2017-07-27 17:08:22 +0800

    add wp-content

--- wp-content/themes/iconic-one/footer.php
+++ wp-content/themes/iconic-one/footer.php
@@ -9,10 +9,21 @@
 	</div><!-- #main .wrapper -->
 	<footer id="colophon" role="contentinfo">
 		<div class="site-info">
-<!--
-		<div class="footercopy"><?php echo get_theme_mod( 'textarea_copy', 'custom footer text left' ); ?></div>
+
+    <div class="footercopy">
+
+<ul class="sitemap clearfix">
+<li id="menu-item-3532" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3532"><a href="https://www.macbed.com/about/">About</a></li>
+<li id="menu-item-4241" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4241"><a href="https://www.macbed.com/dmca/">DMCA Policy</a></li>
+<li id="menu-item-3200" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3200"><a href="https://www.macbed.com/topics/">Topics</a></li>
+<li id="menu-item-3203" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3203"><a href="https://www.macbed.com/contact/">Contact</a></li>
+</ul>
+
+</div>
 		<div class="footercredit"><?php echo get_theme_mod( 'custom_text_right', 'custom footer text right' ); ?></div>
--->
+
+
+
 		<div class="clear"></div>
 		</div><!-- .site-info -->
 		</footer><!-- #colophon -->
